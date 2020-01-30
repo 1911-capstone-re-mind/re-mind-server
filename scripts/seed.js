02078e027a39d052b5cdac6b0938ec787e4b74bb
rtakahashi <lou.takahashi@gmail.com>
2020-01-30 15:26:40 -0600

    created seed file

---
+++ scripts/seed.js
@@ -0,0 +1,59 @@
+const db = require('../server/db');
+const { Activity } = require('../server/db/models')
+
+const activitiesSeed = [
+  {
+    name: 'posture',
+    description: 'Good posture is about more than standing up straight so you can look your best. It is an important part of your long-term health. Making sure that you hold your body the right way can prevent pain, injuries, and other health problems.',
+    frequency: 30,
+    duration: 0
+  },
+  {
+    name: 'movement',
+    description: 'Sitting too much can contribute to health problems. Being less sedentary can lead to lower blood pressure, increased energy, and reduced risk of cardiovascular disease.',
+    frequency: 60,
+    duration: 1
+  },
+  {
+    name: 'eye strain',
+    description: 'Every 20 minutes, look at something 20 feet away for 20 seconds. The 20/20/20 rule was popularized by Dr. Jeff Anshell, a specialist in “vision ergonomics.”',
+    frequency: 20,
+    duration: 20
+  },
+  {
+    name: 'hydration',
+    description: 'Water is your body\'s principal chemical component and makes up about 60 percent of your body weight. Your body depends on water to survive. Lack of water can lead to dehydration — a condition that occurs when you don\'t have enough water in your body to carry out normal functions. Even mild dehydration can drain your energy and make you tired.',
+    frequency: 30,
+    duration: 0
+  },
+  {
+    name: 'mindfulness',
+    description: 'Mindfulness techniques allow us to calm down and to be more receptive than reactive. Whether learning to meditate or merely to tune in with ourselves at various times throughout our day, mindfulness enhances your ability to feel more integrated and to act with integrity. We improve our ability to focus our attention. We are better able to slow the racing thoughts that lead us to engage in limiting or self-sabotaging behaviors. We strengthen our resilience and enhance our capacity to experience the joys of everyday life.',
+    frequency: 30,
+    duration: 0
+  },
+]
+
+
+
+const seed = async () => {
+  await db.sync({force: true})
+  const activities = await Activity.bulkCreate(activitiesSeed)
+
+  console.log(`${activities.length} activities created`)
+}
+
+const runSeed = async () => {
+  try {
+    await seed()
+    console.log('success!')
+  } catch (err) {
+    console.error(err)
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+runSeed()
