faaa4f689801be6bb7845592c25e94742de74f73
Marius M <mariuscm@users.noreply.github.com>
2020-01-30 15:58:24 -0600

    Delete secrets.js

--- secrets.js
+++
@@ -1,6 +0,0 @@
-process.env.GOOGLE_CLIENT_ID =
-  // iOS id
-  "824184418309-5eol061urus9c98t2g8s8i49lrhg2bsu.apps.googleusercontent.com";
-//   "811790070790-4ct3asie31bmmoubjhv3tvg8r8clr5a6.apps.googleusercontent.com";
-process.env.GOOGLE_CLIENT_SECRET = "PzUfQKoELvYuUcE56NYfMyOc";
-process.env.GOOGLE_CALLBACK = "/auth/google/callback";
