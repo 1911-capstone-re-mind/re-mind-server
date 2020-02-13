c420e75385b30a095aad8fed86fa52e50b414677
rtakahashi <lou.takahashi@gmail.com>
2020-02-13 10:38:31 -0600

    removed unused files and routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 module.exports = router;
 
-router.use("/users", require("./users"));
 router.use("/activities",  require("./activities"));
 router.use("/activity-log", require("./activityLog"));
 
